3c207fabe8e3c67c24dfc9bd3e5b62a69ded99c8
Pranjal Tiwari <tiwarip1@msu.edu>
2018-06-25 13:37:17 -0400

    made a few additions to the gui and the plotting function

--- Stocks/new_scripts/gui.py
+++ Stocks/new_scripts/gui.py
@@ -11,6 +11,7 @@ import sys
 import os
 import random
 import pandas as pd
+import datetime as dt
 
 from functions import create_bollinger,daily_close
 
@@ -26,8 +27,8 @@ class App(QWidget):
         self.title = 'Patented Amazing Stock Plotting Program'
         self.left = 0
         self.top = 50
-        self.width = 840
-        self.height = 280
+        self.width = 1920
+        self.height = 1080
         self.initUI()
  
     def initUI(self):
@@ -43,29 +44,29 @@ class App(QWidget):
         
         #Textbox for Stock Symbol
         self.ticker = QLineEdit(self,text='TSLA')
-        self.ticker.move(200,20)
+        self.ticker.move(540,20)
         self.ticker.resize(280,40)
         
         #CheckBox
         self.b1 = QCheckBox("Bolinger Bands",self)
-        self.b1.move(540,20)
+        self.b1.move(860,20)
         self.b1.resize(320,40)
         
         self.b2 = QCheckBox("Optimal Simple Moving Averages",self)
-        self.b2.move(540,60)
+        self.b2.move(860,60)
         self.b2.resize(320,40)
         
         self.b3 = QCheckBox("P/E Ratio",self)
-        self.b3.move(540,100)
+        self.b3.move(860,100)
         self.b3.resize(320,40)
         
         self.b4 = QCheckBox("Oscillator",self)
-        self.b4.move(540,140)
+        self.b4.move(860,140)
         self.b4.resize(320,40)
 
         # Create a quit buttom
         self.button = QPushButton('Quit',self)
-        self.button.move(380,200)
+        self.button.move(860,200)
         self.button.clicked.connect(self.exiting)
         
         
@@ -105,7 +106,10 @@ class PlotCanvas(FigureCanvas):
         
         location=os.path.expanduser('~/Documents/daily_close')
             
-        daily_close(self.ticker,location=location)
+        curr = dt.datetime.now().date()
+        start = curr-dt.timedelta(days=365)
+        
+        daily_close(self.ticker,location=location,start=start)
         
         data = pd.read_csv('{}/{}.csv'.format(location,self.ticker))
         
@@ -113,6 +117,11 @@ class PlotCanvas(FigureCanvas):
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
         ax.plot(data['close'],'k--',alpha=.6)
+        ax.set_xticks([0,len(data['close'])/4,len(data['close'])/2,\
+                       3*len(data['close'])/4,len(data['close'])])
+        ax.set_xticklabels([start,start+dt.timedelta(days=int(len(\
+data['close'])/4)),start+dt.timedelta(days=int(len(data['close'])/2)),start+dt\
+    .timedelta(days=int(3*len(data['close'])/4)),curr],rotation=45)
         ax.set_title('PyQt Matplotlib Example')
         self.draw()
         self.show()
