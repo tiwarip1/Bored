eca3540593e2765bd6ca268f9f73f76f25cee32a
HerpDerp <herpitehderpo@gmail.com>
2018-07-23 18:50:47 -0400

    added a class to show transaction price along with any applicable charges including currency conversions

--- Stocks/new_scripts/analysis.py
+++ Stocks/new_scripts/analysis.py
@@ -1,20 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+from classes import transactions
 
-from functions import bollinger,create_derivative,create_exponential_moving_average,add_rolling_average
-
-df = pd.read_csv('../../../stored_data/TSLA.csv')
-
-df = create_exponential_moving_average(df,window=20)
-df = create_derivative(df,'20ema')
-df = add_rolling_average(df,20,'dma','d20ema')
-
-#print(df)
-
-#plt.plot(df['20ema'])
-plt.plot(df['d20ema'][-100:])
-plt.plot(df['20dma'][-100:])
-
-#ran = input("Over what range? ")
-
-#bollinger(ran,True)
+thing = transactions(symbol='MAXR',number_stocks=30,convert=False)
+thing.buy(67.83)
+thing.sell(67.25,20)
+thing.override=True
+thing.sell(69.97,10)
+print(thing.total())
--- Stocks/new_scripts/classes.py
+++ Stocks/new_scripts/classes.py
@@ -1,33 +1,80 @@
-
+import requests
+import bs4 as bs
 
 class transactions():
+    '''
+    This is a class which was made to generalize buying and selling stocks and
+    includes any fees that would apply. This is to be used in a loop to 
+    determine whether certain alerts are profitable in the end or not and 
+    converts CAD to USD and vice versa if trading occurs in different 
+    currencies
+    '''
     
-    def __init__(self,symbol='NFLX',convert=False,number_stocks=100):
-        
+    def __init__(self,symbol='NFLX',convert=False,number_stocks=0):
+        #Initializes varaibles to be used across class
         self.symbol=symbol
-        self.convery=convert
+        self.convert=convert
         self.buying=[]
         self.selling=[]
         self.next_action='buy'
         self.number_stocks = number_stocks
+        self.service_charge = 7
+        self.override = False
         
-    def buying(self,buy=0):
-        
-        if self.next_action=='buy':
+    def buy(self,buy=0,number_stocks=None):
+        #If the buying action is requested, the class will save the price        
+        if self.next_action=='buy' or self.override:
             
-            self.buying.append(buy)
+            if number_stocks==None:
+                number_stocks=self.number_stocks
+            else:
+                self.number_stocks=number_stocks+self.number_stocks
+                
+            if self.convert:
+                buy = self.add_fees(buy)
+            self.buying.append(buy*number_stocks+self.service_charge)
             self.next_action='sell'
+            
         else:
+            #Prevent selling twice
             print('problem with buying')
         
-    def selling(self,sell=0):
-        
-        if self.next_action=='sell':
-            self.selling.append(sell)
+    def sell(self,sell=0,number_stocks = None):
+        #If the selling action is requested, the class will save the price        
+        if self.next_action=='sell' or self.override:
+            
+            if number_stocks==None:
+                number_stocks=self.number_stocks
+            else:
+                self.number_stocks=-number_stocks+self.number_stocks
+            
+            if self.convert:
+                sell = self.add_fees(sell)
+            self.selling.append(sell*number_stocks-self.service_charge)
             self.next_action='buy'
+            
         else:
+            #Prevent buying twice
             print('problem with selling')
             
-    def add_fees(self):
-        
-        
+    def get_conversion_rate(self):
+        #Gets a realtime conversion rate at market value
+        resp=requests.get('https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=CAD')
+        soup = bs.BeautifulSoup(resp.text,'lxml')
+        table = soup.find('span',{'class':'uccResultAmount'})
+        self.conversion = float(str(table)[30:-7])
+        self.cibc_conv = self.conversion+.05
+            
+    def add_fees(self,price):
+        #Adds any fees that would come from making a transaction, buy or sell
+        self.get_conversion_rate()
+        if self.next_action=='buy':
+            price=price-(price*self.conversion-price*self.cibc_conv)
+        else:
+            price=+price+(price*self.conversion-price*self.cibc_conv)
+            
+        return price
+    
+    def total(self):
+        #Returns total profit
+        return -sum(self.buying)+sum(self.selling)
